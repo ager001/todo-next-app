dcf2607ed6bfa6db4ab10f6260665dcbe698e121
ager001 <152964009+ager001@users.noreply.github.com>
2026-01-07 21:37:59 +0300

    refactor: update app title and remove unused styles; add Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex py-3 flex-wrap justify-around'>
+      <h1>Todo APP</h1>
+    </div>
+  )
+}
+
+export default Navbar
